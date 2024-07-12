915a7d6a555105d109860caa46af30903113510f
silva-fmanu <silvafarias.manu07@gmail.com>
2024-07-12 08:42:48 -0300

    movendo posts e detalhes post para a pasta pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom' 
-import "./components/DetalhesPost"
-import DetalhesPost from './components/DetalhesPost';
+import DetalhesPost from './pages/DetalhesPost';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Posts from './components/Posts';
+import Posts from './pages/Posts';
 
 function RoutesApp(){
     return(
